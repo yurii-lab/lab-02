c6bd33afd41a83304f318e74697e048600d8c4ee
yurii-lab <yurii.artemenko@tetalab.com>
2025-05-28 17:35:05 +0300

    Update mpi_hello_world.c

--- mpi_hello_world.c
+++ mpi_hello_world.c
@@ -1,20 +1,29 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-	MPI_Init(NULL, NULL);
+    MPI_Init(NULL, NULL);
 
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
 
-	printf("Hello world from processor %s, rank %d out of %dprocessors\n", processor_name, world_rank, world_size);
+    char filename[256];
+    snprintf(filename, sizeof(filename), "/tmp/mpi_stat_%s_%d.txt", processor_name, world_rank);
+    FILE* fp = fopen(filename, "w");
+    if (fp != NULL) {
+        fprintf(fp, "Rank %d of %d on processor %s\n", world_rank, world_size, processor_name);
+        fclose(fp);
+    }
 
-	MPI_Finalize();
+    MPI_Finalize();
+    return 0;
 }
